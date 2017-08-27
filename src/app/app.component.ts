17df95972c8f1b46dadfd2e3de94b3c476cd42f7
Tyler Peterson <TheNewStyles@github.com>
2017-08-27 10:30:57 -0600

    Add new component. Introduction to event binding, class binding, and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+
+  template: `<h1 [class]="titleClass">Hey guys</h1>
+
+            <button [disabled]="buttonStatus == 'enabled'">{{test}}</button>
+            <button (click)="myEvent($event)">My button</button>
+            `,
+
+  //styleUrls: ['./app.component.css']
+
+  styles: [`
+    .red {
+      color: red;
+    }
+  `]
 })
+
 export class AppComponent {
-  title = 'app';
+  buttonStatus = 'enabled';
+  test= 'test this is weird';
+  titleClass="red";
+
+  myEvent(event){
+    console.log(event);
+  }
+
 }
